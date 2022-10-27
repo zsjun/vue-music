7be4ab3c9fc62bb7197905ef0f6730a89bd7984c
zsj <zousjun@126.com>
2022-10-27 11:17:54 +0800

    初始化

--- src/router/index1.js
+++ src/router/index1.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const Recommend = () => import('@/views/recommend'/* webpackChunkName: "recommend" */)
-const Singer = () => import('@/views/singer'/* webpackChunkName: "singer" */)
-const TopList = () => import('@/views/top-list'/* webpackChunkName: "top-list" */)
-const Search = () => import('@/views/search'/* webpackChunkName: "search" */)
-const SingerDetail = () => import('@/views/singer-detail'/* webpackChunkName: "singer-detail" */)
-const Album = () => import('@/views/album'/* webpackChunkName: "album" */)
-const TopDetail = () => import('@/views/top-detail'/* webpackChunkName: "top-detail" */)
-const UserCenter = () => import('@/views/user-center'/* webpackChunkName: "user-center" */)
+const Recommend = () =>
+  import('@/views/recommend' /* webpackChunkName: "recommend" */)
+const Singer = () => import('@/views/singer' /* webpackChunkName: "singer" */)
+const TopList = () =>
+  import('@/views/top-list' /* webpackChunkName: "top-list" */)
+const Search = () => import('@/views/search' /* webpackChunkName: "search" */)
+const SingerDetail = () =>
+  import('@/views/singer-detail' /* webpackChunkName: "singer-detail" */)
+const Album = () => import('@/views/album' /* webpackChunkName: "album" */)
+const TopDetail = () =>
+  import('@/views/top-detail' /* webpackChunkName: "top-detail" */)
+const UserCenter = () =>
+  import('@/views/user-center' /* webpackChunkName: "user-center" */)
 
 const routes = [
   {
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,67 @@
+const Recommend = () =>
+  import('@/views/recommend' /* webpackChunkName: "recommend" */)
+const Singer = () => import('@/views/singer' /* webpackChunkName: "singer" */)
+const TopList = () =>
+  import('@/views/top-list' /* webpackChunkName: "top-list" */)
+const Search = () => import('@/views/search' /* webpackChunkName: "search" */)
+const SingerDetail = () =>
+  import('@/views/singer-detail' /* webpackChunkName: "singer-detail" */)
+const Album = () => import('@/views/album' /* webpackChunkName: "album" */)
+const TopDetail = () =>
+  import('@/views/top-detail' /* webpackChunkName: "top-detail" */)
+const UserCenter = () =>
+  import('@/views/user-center' /* webpackChunkName: "user-center" */)
+
 const routes = [
   {
     path: '/',
-    component: () => import('../views/SampleView.vue')
-    // children: [
-    //   {
-    //     name: "Home",
-    //     path: "home",
-    //     component: () => import("../views/SampleView.vue"),
-    //   },
-    // ],
+    redirect: '/recommend'
   },
   {
-    path: '/home',
-    component: () => import('../views/SampleView.vue')
-    // children: [
-    //   {
-    //     name: "Home",
-    //     path: "home",
-    //     component: () => import("../views/SampleView.vue"),
-    //   },
-    // ],
+    path: '/recommend',
+    component: Recommend,
+    children: [
+      {
+        path: ':id',
+        component: Album
+      }
+    ]
+  },
+  {
+    path: '/singer',
+    component: Singer,
+    children: [
+      {
+        path: ':id',
+        component: SingerDetail
+      }
+    ]
+  },
+  {
+    path: '/top-list',
+    component: TopList,
+    children: [
+      {
+        path: ':id',
+        component: TopDetail
+      }
+    ]
+  },
+  {
+    path: '/search',
+    component: Search,
+    children: [
+      {
+        path: ':id',
+        component: SingerDetail
+      }
+    ]
+  },
+  {
+    path: '/user',
+    components: {
+      user: UserCenter
+    }
   }
 ]
-
 export default routes
